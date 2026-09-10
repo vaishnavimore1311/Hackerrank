78eb33c011e51f6e1162315549200297b324e3e4
Vaishnavi More <vaishnavimore1311@gmail.com>
2026-09-10 16:10:15 +0530

    Add files via upload

---
+++ difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+english = set(map(int, input().split()))
+
+m = int(input())
+french = set(map(int, input().split()))
+
+only_english = english.difference(french)
+
+print(len(only_english))
