a5659ab53f78f86cfcf4b6fa73a7a3a4666d02b1
Vaishnavi More <vaishnavimore1311@gmail.com>
2026-09-11 13:18:46 +0530

    Add files via upload

---
+++ Any_all.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+nums = list(map(int, input().split()))
+print(all(x > 0 for x in nums) and any(str(x) == str(x)[::-1] for x in nums))
