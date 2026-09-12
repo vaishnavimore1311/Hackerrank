de1402236bb674615c438b318db6809fe4421c1e
Vaishnavi More <vaishnavimore1311@gmail.com>
2026-09-12 10:19:43 +0530

    Delete union.py

--- union.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-english = set(map(int, input().split()))
-m = int(input())
-french = set(map(int, input().split()))
-result = english.union(french)
-print(len(result))
